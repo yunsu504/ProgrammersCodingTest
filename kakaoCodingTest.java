71f919139ac1498f706e7fb12abfcf44b2ce2a30
Yunsoo Kim <73781553+yunsu504@users.noreply.github.com>
2021-01-13 21:02:22 +0900

    2019 카카오개발자겨울인턴십>크레인인형뽑기게임

--- kakaoCodingTest.java
+++ kakaoCodingTest.java
@@ -1,173 +1,86 @@
 package com.oopsw.test;
 /*
 코딩테스트 연습
-2020 카카오 인턴십
-키패드 누르기
-도움말
-컴파일 옵션
-[카카오 인턴] 키패드 누르기
+2019 카카오 개발자 겨울 인턴십
+크레인 인형뽑기 게임
+크레인 인형뽑기 게임
 문제 설명
-스마트폰 전화 키패드의 각 칸에 다음과 같이 숫자들이 적혀 있습니다.
+게임개발자인 죠르디는 크레인 인형뽑기 기계를 모바일 게임으로 만들려고 합니다.
+죠르디는 게임의 재미를 높이기 위해 화면 구성과 규칙을 다음과 같이 게임 로직에 반영하려고 합니다.
 
-kakao_phone1.png
+crane_game_101.png
 
-이 전화 키패드에서 왼손과 오른손의 엄지손가락만을 이용해서 숫자만을 입력하려고 합니다.
-맨 처음 왼손 엄지손가락은 * 키패드에 오른손 엄지손가락은 # 키패드 위치에서 시작하며, 엄지손가락을 사용하는 규칙은 다음과 같습니다.
+게임 화면은 1 x 1 크기의 칸들로 이루어진 N x N 크기의 정사각 격자이며 위쪽에는 크레인이 있고 오른쪽에는 바구니가 있습니다. (위 그림은 5 x 5 크기의 예시입니다). 각 격자 칸에는 다양한 인형이 들어 있으며 인형이 없는 칸은 빈칸입니다. 모든 인형은 1 x 1 크기의 격자 한 칸을 차지하며 격자의 가장 아래 칸부터 차곡차곡 쌓여 있습니다. 게임 사용자는 크레인을 좌우로 움직여서 멈춘 위치에서 가장 위에 있는 인형을 집어 올릴 수 있습니다. 집어 올린 인형은 바구니에 쌓이게 되는 데, 이때 바구니의 가장 아래 칸부터 인형이 순서대로 쌓이게 됩니다. 다음 그림은 [1번, 5번, 3번] 위치에서 순서대로 인형을 집어 올려 바구니에 담은 모습입니다.
 
-엄지손가락은 상하좌우 4가지 방향으로만 이동할 수 있으며 키패드 이동 한 칸은 거리로 1에 해당합니다.
-왼쪽 열의 3개의 숫자 1, 4, 7을 입력할 때는 왼손 엄지손가락을 사용합니다.
-오른쪽 열의 3개의 숫자 3, 6, 9를 입력할 때는 오른손 엄지손가락을 사용합니다.
-가운데 열의 4개의 숫자 2, 5, 8, 0을 입력할 때는 두 엄지손가락의 현재 키패드의 위치에서 더 가까운 엄지손가락을 사용합니다.
-4-1. 만약 두 엄지손가락의 거리가 같다면, 오른손잡이는 오른손 엄지손가락, 왼손잡이는 왼손 엄지손가락을 사용합니다.
-순서대로 누를 번호가 담긴 배열 numbers, 왼손잡이인지 오른손잡이인 지를 나타내는 문자열 hand가 매개변수로 주어질 때, 각 번호를 누른 엄지손가락이 왼손인 지 오른손인 지를 나타내는 연속된 문자열 형태로 return 하도록 solution 함수를 완성해주세요.
+crane_game_102.png
+
+만약 같은 모양의 인형 두 개가 바구니에 연속해서 쌓이게 되면 두 인형은 터뜨려지면서 바구니에서 사라지게 됩니다. 위 상태에서 이어서 [5번] 위치에서 인형을 집어 바구니에 쌓으면 같은 모양 인형 두 개가 없어집니다.
+
+crane_game_103.gif
+
+크레인 작동 시 인형이 집어지지 않는 경우는 없으나 만약 인형이 없는 곳에서 크레인을 작동시키는 경우에는 아무런 일도 일어나지 않습니다. 또한 바구니는 모든 인형이 들어갈 수 있을 만큼 충분히 크다고 가정합니다. (그림에서는 화면표시 제약으로 5칸만으로 표현하였음)
+
+게임 화면의 격자의 상태가 담긴 2차원 배열 board와 인형을 집기 위해 크레인을 작동시킨 위치가 담긴 배열 moves가 매개변수로 주어질 때, 크레인을 모두 작동시킨 후 터트려져 사라진 인형의 개수를 return 하도록 solution 함수를 완성해주세요.
 
 [제한사항]
-numbers 배열의 크기는 1 이상 1,000 이하입니다.
-numbers 배열 원소의 값은 0 이상 9 이하인 정수입니다.
-hand는 "left" 또는 "right" 입니다.
-"left"는 왼손잡이, "right"는 오른손잡이를 의미합니다.
-왼손 엄지손가락을 사용한 경우는 L, 오른손 엄지손가락을 사용한 경우는 R을 순서대로 이어붙여 문자열 형태로 return 해주세요.
+board 배열은 2차원 배열로 크기는 5 x 5 이상 30 x 30 이하입니다.
+board의 각 칸에는 0 이상 100 이하인 정수가 담겨있습니다.
+0은 빈 칸을 나타냅니다.
+1 ~ 100의 각 숫자는 각기 다른 인형의 모양을 의미하며 같은 숫자는 같은 모양의 인형을 나타냅니다.
+moves 배열의 크기는 1 이상 1,000 이하입니다.
+moves 배열 각 원소들의 값은 1 이상이며 board 배열의 가로 크기 이하인 자연수입니다.
 입출력 예
-numbers	hand	result
-[1, 3, 4, 5, 8, 2, 1, 4, 5, 9, 5]	"right"	"LRLLLRLLRRL"
-[7, 0, 8, 2, 8, 3, 1, 5, 7, 6, 2]	"left"	"LRLLRRLLLRR"
-[1, 2, 3, 4, 5, 6, 7, 8, 9, 0]	"right"	"LLRLLRLLRL"
+board	moves	result
+[[0,0,0,0,0],[0,0,1,0,3],[0,2,5,0,1],[4,2,4,4,2],[3,5,1,3,1]]	[1,5,3,5,1,2,1,4]	4
 입출력 예에 대한 설명
 입출력 예 #1
 
-순서대로 눌러야 할 번호가 [1, 3, 4, 5, 8, 2, 1, 4, 5, 9, 5]이고, 오른손잡이입니다.
+인형의 처음 상태는 문제에 주어진 예시와 같습니다. 크레인이 [1, 5, 3, 5, 1, 2, 1, 4] 번 위치에서 차례대로 인형을 집어서 바구니에 옮겨 담은 후, 상태는 아래 그림과 같으며 바구니에 담는 과정에서 터트려져 사라진 인형은 4개 입니다.*/
+import java.util.ArrayList;
+import java.util.List;
 
-왼손 위치	오른손 위치	눌러야 할 숫자	사용한 손	설명
-*	#	1	L	1은 왼손으로 누릅니다.
-1	#	3	R	3은 오른손으로 누릅니다.
-1	3	4	L	4는 왼손으로 누릅니다.
-4	3	5	L	왼손 거리는 1, 오른손 거리는 2이므로 왼손으로 5를 누릅니다.
-5	3	8	L	왼손 거리는 1, 오른손 거리는 3이므로 왼손으로 8을 누릅니다.
-8	3	2	R	왼손 거리는 2, 오른손 거리는 1이므로 오른손으로 2를 누릅니다.
-8	2	1	L	1은 왼손으로 누릅니다.
-1	2	4	L	4는 왼손으로 누릅니다.
-4	2	5	R	왼손 거리와 오른손 거리가 1로 같으므로, 오른손으로 5를 누릅니다.
-4	5	9	R	9는 오른손으로 누릅니다.
-4	9	5	L	왼손 거리는 1, 오른손 거리는 2이므로 왼손으로 5를 누릅니다.
-5	9	-	-	
-따라서 "LRLLLRLLRRL"를 return 합니다.
-
-입출력 예 #2
-
-왼손잡이가 [7, 0, 8, 2, 8, 3, 1, 5, 7, 6, 2]를 순서대로 누르면 사용한 손은 "LRLLRRLLLRR"이 됩니다.
-
-입출력 예 #3
-
-오른손잡이가 [1, 2, 3, 4, 5, 6, 7, 8, 9, 0]를 순서대로 누르면 사용한 손은 "LLRLLRLLRL"이 됩니다.*/
 public class kakaoCodingTest {
-	class Solution {
-	    public String solution(int[] numbers, String hand) {
-	       		String answer = "";
-			// 어느 손잡이?
-			switch(hand){
-			case "right": hand = "R"; break;
-			case "left": hand = "L"; break;
+
+	  public int solution(int[][] board, int[] moves) {
+	        int answer = 0;
+	        int getNumber = 0;
+			List stack = new ArrayList();
+			stack.add(0);
+
+			for (int i = 0; i < moves.length; i++){
+				for (int j = 0; j < board.length; j++){
+					if (0 != board[j][moves[i]-1]){
+						getNumber = board[j][moves[i]-1];
+						if (stack.get(stack.size()-1).equals(getNumber)){
+							stack.remove(stack.size()-1);
+							answer = answer + 2;
+	                        board[j][moves[i]-1] = 0;	
+							break;
+						}else{
+							stack.add(getNumber);
+							board[j][moves[i]-1] = 0;	
+							break;
+						}
+					}
+				}
 			}
-			// 손의 최신위치 기억
-			int beforeL = 10; //10 = * 
-			int beforeR = 12; //12 = #
-			
-			//numbers로 어떤 숫자를 입력해야 하는지  number
-			for (int i = 0; i < numbers.length ; i++){
-				int number = numbers[i];
-			//	System.out.println("왼손위치 :" +beforeL + ", 오른손위치 :" + beforeR + ", number : " + number);
-				
-				int countL = 0;
-				int countR = 0;
-				switch (number){
-				case 1:
-				case 4:
-				case 7:
-					answer += "L";
-					beforeL = number;
-					break;
-				case 3:
-				case 6:
-				case 9:
-					answer += "R";
-					beforeR = number;
-					break;
-				default:
-				
-					if (number == 0) {
-						number = 11;
-					}
-					//왼손check
-					if (number > beforeL){
-						for (int j = beforeL+1; j <= number; j++){
-							countL ++;
-						}
-			
-		        //  System.out.println("왼손카운터1: " +countL);
-					}else if(number < beforeL){
-						for (int j = beforeL-1; j >= number; j--){
-								countL ++;
-						}
-						
-			    //  System.out.println("왼손카운터2: " +countL);
-					}
-					//오른손check				
-					if (number > beforeR){
-						for (int j = beforeR+1; j <= number; j++){
-							countR ++;
-						}
-				//	System.out.println("오른손카운터1: " +countR);
-					}else if(number < beforeR){
-						for (int j = beforeR-1; j >= number; j--){
-								countR ++;
-						}
-				//	System.out.println("오른카운터2: " +countR);
-					}
-				//	System.out.println(countL/3+countL%3);
-				//	System.out.println(countR/3+countR%3);
-					if (countL/3+countL%3 > countR/3+countR%3){
-						answer += "R";
-						beforeR = number;
-						
-					}else if (countL/3+countL%3 < countR/3+countR%3){
-						answer += "L";
-						beforeL = number;
-					}else {
-						answer += hand;
-	                    switch(hand){
-						case "R": beforeR = number; break;
-						case "L": beforeL = number; break;
-						}
-					}		
-				}//switch
-			}//for	
-			return answer;		
-		}//main
-	}//class
-}
-	/*실행 결과
-	채점을 시작합니다.
-	정확성  테스트
-	테스트 1 〉	통과 (2.40ms, 52.6MB)
-	테스트 2 〉	통과 (1.36ms, 51.6MB)
-	테스트 3 〉	통과 (1.75ms, 52.3MB)
-	테스트 4 〉	통과 (2.28ms, 52.2MB)
-	테스트 5 〉	통과 (1.44ms, 52.3MB)
-	테스트 6 〉	통과 (2.17ms, 53.9MB)
-	테스트 7 〉	통과 (2.54ms, 52.1MB)
-	테스트 8 〉	통과 (2.62ms, 52.8MB)
-	테스트 9 〉	통과 (2.78ms, 53.2MB)
-	테스트 10 〉	통과 (2.90ms, 52.3MB)
-	테스트 11 〉	통과 (2.61ms, 52.9MB)
-	테스트 12 〉	통과 (2.42ms, 53MB)
-	테스트 13 〉	통과 (4.83ms, 52.3MB)
-	테스트 14 〉	통과 (2.93ms, 51.7MB)
-	테스트 15 〉	통과 (3.46ms, 53.1MB)
-	테스트 16 〉	통과 (3.23ms, 53.7MB)
-	테스트 17 〉	통과 (4.20ms, 53.5MB)
-	테스트 18 〉	통과 (4.33ms, 53.6MB)
-	테스트 19 〉	통과 (4.28ms, 54.8MB)
-	테스트 20 〉	통과 (6.18ms, 53.5MB)
-	채점 결과
-	정확성: 100.0
-	합계: 100.0 / 100.0*/
+	    return answer ;
+	    }
+	}
+/*실행 결과
+채점을 시작합니다.
+정확성  테스트
+테스트 1 〉	통과 (0.06ms, 53.6MB)
+테스트 2 〉	통과 (0.08ms, 53.6MB)
+테스트 3 〉	통과 (0.07ms, 52.7MB)
+테스트 4 〉	통과 (1.33ms, 52.6MB)
+테스트 5 〉	통과 (0.06ms, 53.3MB)
+테스트 6 〉	통과 (0.07ms, 52.6MB)
+테스트 7 〉	통과 (0.18ms, 52MB)
+테스트 8 〉	통과 (0.41ms, 52.2MB)
+테스트 9 〉	통과 (0.47ms, 52.3MB)
+테스트 10 〉	통과 (0.45ms, 52.1MB)
+테스트 11 〉	통과 (7.89ms, 52.9MB)
+채점 결과
+정확성: 100.0
+합계: 100.0 / 100.0*/
